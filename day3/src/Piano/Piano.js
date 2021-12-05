9b50515be27ef9cb68e5f7d33aa8af3f0ac7569a
pouletor <zesuila5@hotmail.com>
2021-12-05 19:44:39 +0100

    Update svg piano for keyboard navigation

--- day3/src/Piano/Piano.js
+++ day3/src/Piano/Piano.js
@@ -6,6 +6,41 @@ function Piano() {
   wrapper.className = styles.piano;
   wrapper.innerHTML = PianoSvg;
 
+  const whiteKeys = wrapper.querySelectorAll(".white-keys");
+  whiteKeys.forEach((key) => {
+    key.classList.add(styles["white-keys"]);
+  });
+
+  const blackKeys = wrapper.querySelectorAll(".black-keys");
+  blackKeys.forEach((key) => {
+    key.classList.add(styles["black-keys"]);
+  });
+
+  const handleToggleClass = (element) => () => {
+    element.classList.toggle(styles["piano__key--clicked"]);
+  };
+
+  const handleRemoveClass = (element) => () => {
+    element.classList.remove(styles["piano__key--clicked"]);
+  };
+
+  const handleEnterPress = (element) => (event) => {
+    if (event.key === "Enter") {
+      handleToggleClass(element)();
+    } else {
+      // prevent bug when smashing Enter and Tab simutaneously
+      handleRemoveClass(element)();
+    }
+  };
+
+  const keys = wrapper.querySelectorAll("path");
+  keys.forEach((key) => {
+    key.addEventListener("mousedown", handleToggleClass(key));
+    key.addEventListener("mouseup", handleToggleClass(key));
+    key.addEventListener("keydown", handleEnterPress(key));
+    key.addEventListener("keyup", handleEnterPress(key));
+  });
+
   return wrapper;
 }
 
