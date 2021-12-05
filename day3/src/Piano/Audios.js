3d467bb7339e18fd2ee389364bfcfe75ee0a55b6
pouletor <zesuila5@hotmail.com>
2021-12-05 21:47:17 +0100

    Day 3 JS part

---
+++ day3/src/Piano/Audios.js
@@ -0,0 +1,8 @@
+// import audio1 from "../assets/audio/key-1.mp3";
+import path from "path";
+
+function Audios() {
+  console.log(path.resolve(__dirname, "src/assets/audio"));
+}
+
+export default Audios;
--- day3/src/Piano/Piano.js
+++ day3/src/Piano/Piano.js
@@ -1,6 +1,18 @@
 import styles from "./Piano.module.css";
 import PianoSvg from "../assets/svg/piano.svg";
 
+// https://stackoverflow.com/questions/29421409/how-to-load-all-files-in-a-directory-using-webpack-without-require-statements/30652110#30652110 and https://medium.com/dailyjs/leveraging-webpack-power-to-import-all-files-from-one-folder-cddedd3201b3
+function requireAll(r) {
+  return r.keys().map(r);
+}
+
+const EVENT = {
+  MOUSEDOWN: "mousedown",
+  MOUSEUP: "mouseup",
+  KEYDOWN: "keydown",
+  KEYUP: "keyup",
+};
+
 function Piano() {
   const wrapper = document.createElement("div");
   wrapper.className = styles.piano;
@@ -16,29 +28,69 @@ function Piano() {
     key.classList.add(styles["black-keys"]);
   });
 
-  const handleAddClass = (element) => () => {
+  const audiosFile = requireAll(
+    require.context("../assets/audio/", true, /.mp3$/)
+  );
+
+  const addClass = (element) => {
     element.classList.add(styles["piano__key--clicked"]);
   };
 
-  const handleRemoveClass = (element) => () => {
+  const removeClass = (element) => {
     element.classList.remove(styles["piano__key--clicked"]);
   };
 
-  const handleEnterPress = (element) => (event) => {
+  const audiosArray = [];
+  audiosFile.forEach((file) => {
+    const audioObj = new Audio(file);
+    audioObj.loop = true;
+    audiosArray.push(audioObj);
+  });
+
+  const playAudioFile = (element) => {
+    const index = element.id;
+    addClass(element);
+    if (audiosArray[index].readyState >= 2) {
+      audiosArray[index].play();
+    }
+  };
+
+  const pauseAudioFile = (element) => {
+    const index = element.id;
+    removeClass(element);
+    audiosArray[index].pause();
+  };
+
+  const handleMouseEvent = (element) => (event) => {
+    if (event.type === EVENT.MOUSEDOWN) {
+      playAudioFile(element);
+    }
+    if (event.type === EVENT.MOUSEUP) {
+      pauseAudioFile(element);
+    }
+  };
+
+  const handleKeyEvent = (element) => (event) => {
     if (event.key === "Enter") {
-      handleAddClass(element)();
+      if (event.type === EVENT.KEYDOWN) {
+        playAudioFile(element);
+      }
+      if (event.type === EVENT.KEYUP) {
+        pauseAudioFile(element);
+      }
     } else {
       // prevent bug when smashing Enter and Tab simutaneously
-      handleRemoveClass(element)();
+      pauseAudioFile(element);
     }
   };
 
   const keys = wrapper.querySelectorAll("path");
-  keys.forEach((key) => {
-    key.addEventListener("mousedown", handleAddClass(key));
-    key.addEventListener("mouseup", handleRemoveClass(key));
-    key.addEventListener("keydown", handleEnterPress(key));
-    key.addEventListener("keyup", handleEnterPress(key));
+  keys.forEach((key, index) => {
+    key.setAttribute("id", index);
+    key.addEventListener(EVENT.MOUSEDOWN, handleMouseEvent(key));
+    key.addEventListener(EVENT.MOUSEUP, handleMouseEvent(key));
+    key.addEventListener(EVENT.KEYDOWN, handleKeyEvent(key));
+    key.addEventListener(EVENT.KEYUP, handleKeyEvent(key));
   });
 
   return wrapper;
